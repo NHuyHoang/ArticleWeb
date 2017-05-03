b1e8aaf5cee0b68ce7fab133d8f02f951925a49f
Hoang Nguyen <Hoang Nguyen>
2017-05-03 16:51:29 +0700

    2nd push

--- articleweb/src/main/java/articleweb/dao/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package articleweb.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import articleweb.News;
-
-public interface NewsRepository extends CrudRepository<News,Integer>{
-	
-}
--- articleweb/src/main/webapp/WEB-INF/jsp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello spring!</h2>
-</body>
-</html>
